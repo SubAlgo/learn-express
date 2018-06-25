1655cd85fbaebf6fc9adebb3082c8a551a37a608
korn <kornchaval@gmail.com>
2018-06-25 21:16:43 +0700

    [clip 3-9] set ENV

--- config/express.js
+++ config/express.js
@@ -1,7 +1,28 @@
 const express  = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+const bodyParser = require('body-parser')
 
 module.exports = () => {
     const app = express()
+
+    if (process.env.NODE_ENV === 'development') {
+        app.use(morgan('dev'))
+    } else {
+        app.use(compression)
+    }
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }))
+    
+    app.use(bodyParser.json())
+
+
     require('../app/routes/index.routes')(app)
     return app
 }
+
+
+//set NODE_ENV=development
+//set NODE_ENV=production
